4ed6bbd58065f1bb6df364f368225c2a1e774bd9
pr3s3n7 <dragos20@yandex.ru>
2022-06-12 21:34:08 +0300

    feat: update getCpusOS.js file

--- src/SystemOperations/Operations/getCpusOS.js
+++ src/SystemOperations/Operations/getCpusOS.js
@@ -1,5 +1,13 @@
 import { cpus } from 'os'
 
 export const getCpusOS = async() => {
-    return console.log(cpus())
+    const cpuInfo = cpus()
+    const model = cpuInfo[0].model
+    console.log(
+        `CPU Count: ${cpuInfo.length}\nCPU Model: ${model}`
+    )
+    return cpuInfo.map(cpu => console.log({
+        model: cpu.model,
+        speed: cpu.speed / 1000 + 'GHz'
+    }))
 }
